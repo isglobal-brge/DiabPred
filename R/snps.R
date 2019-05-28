623c6b66d9e30d659b54f42952b73fc8495173a3
Akazhiel <34244350+Akazhiel@users.noreply.github.com>
2019-05-28 11:25:33 +0200

    update snps.Rd

--- R/snps.R
+++ R/snps.R
@@ -3,19 +3,15 @@
 #' @title SNP info
 #' @description This dataset consists on the SNPs that were used to build the deep learning model used for prediction.
 #'   
+#' @format A data.frame consisting of 8573 rows and 6 columns
 #' 
 #' \describe{ 
-#'   \item \code{chr}{Chromosome where the SNP is located}
-#'   
-#'   \item \code{snp_id}{SNP reference ID}
-#'   
-#'   \item \code{chr_start}{Start position of the SNP in the chromosome}
-#'   
-#'   \item \code{chr_end}{End position of the SNP in the chromosome}
-#'   
-#'   \item \code{ref}{Reference allele of the SNP}
-#'   
-#'   \item \code{alt}{Alternative allele of the SNP}
+#'   \item{chr}{Chromosome where the SNP is located}
+#'   \item{snp_id}{SNP reference ID}
+#'   \item{chr_start}{Start position of the SNP in the chromosome}
+#'   \item{chr_end}{End position of the SNP in the chromosome}
+#'   \item{ref}{Reference allele of the SNP}
+#'   \item{alt}{Alternative allele of the SNP}
 #'   }
 #'   
 #'   
